784494d64495fc51af95510ac5d29d65ebed21f9
Nikodem Adamczyk <adamczyknikus@gmail.com>
2023-11-20 10:53:23 +0100

    Changing integration tests

--- oolab/src/test/java/agh/ics/oop/model/RectangularMapIntegrationTest.java
+++ oolab/src/test/java/agh/ics/oop/model/RectangularMapIntegrationTest.java
@@ -1,51 +1,137 @@
 package agh.ics.oop.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RectangularMapIntegrationTest {
-    private RectangularMap map;
+class RectangularMapIntegrationTest {
 
-    @BeforeEach
-    void setUp() {
-        map = new RectangularMap(5, 5);
+    @Test
+    void canMoveTo() throws PositionAlreadyOccupiedException {
+        RectangularMap map = new RectangularMap(4,4);
+        assertFalse(map.canMoveTo(new Vector2d(0,-1)));
     }
 
     @Test
-    void testCanMoveTo() {
-        assertTrue(map.canMoveTo(new Vector2d(3, 3)));
-        assertFalse(map.canMoveTo(new Vector2d(6, 6))); // Poza granicami mapy
+    void canMoveTo2()  {
+        RectangularMap map = new RectangularMap(4,4);
+        try {
+            map.place(new Animal(new Vector2d(1, 1)));
+            assertFalse(map.canMoveTo(new Vector2d(1,1)));
+        }catch (PositionAlreadyOccupiedException ex){
+            System.err.println(ex.getMessage());
+        }
     }
 
     @Test
-    void testPlaceAndIsOccupied() {
-        Animal animal = new Animal(new Vector2d(2, 2));
-        assertTrue(map.place(animal));
-        assertTrue(map.isOccupied(new Vector2d(2, 2)));
+    void place() {
+        RectangularMap map = new RectangularMap(3,3);
+        try {
+            assertTrue(map.place(new Animal(new Vector2d(1,1))));
+        }catch (PositionAlreadyOccupiedException ex){
+            System.err.println(ex.getMessage());
+        }
+
     }
 
     @Test
-    void testObjectAt() {
-        Animal animal = new Animal(new Vector2d(2, 2));
-        map.place(animal);
-        assertEquals(animal, map.objectAt(new Vector2d(2, 2)));
-        assertNull(map.objectAt(new Vector2d(3, 3))); // Brak obiektu na tej pozycji
-    }
-    @Test
-    void testGetElements() {
-        Animal animal1 = new Animal(new Vector2d(1, 1));
-        Animal animal2 = new Animal(new Vector2d(2, 2));
-        map.place(animal1);
-        map.place(animal2);
-        assertEquals(2, map.getElements().size());
+    void place2() {
+        RectangularMap map = new RectangularMap(3,3);
+        try {
+            assertFalse(map.place(new Animal(new Vector2d(-1, 1))));
+        }catch (PositionAlreadyOccupiedException ex){
+            System.err.println(ex.getMessage());
+        }
+
     }
 
     @Test
-    void testMove() {
-        Animal animal = new Animal(new Vector2d(2, 2));
-        map.place(animal);
-        map.move(animal, MoveDirection.FORWARD);
-        assertNotNull(map.objectAt(new Vector2d(2, 3)));
+    void move() {
+        RectangularMap map = new RectangularMap(4,4);
+        Animal animal=new Animal(new Vector2d(1,1));
+        try {
+            map.place(animal);
+        }catch (PositionAlreadyOccupiedException ex){
+            System.err.println(ex.getMessage());
+        }
+        map.move(animal,MoveDirection.FORWARD);
+        Vector2d expected=new Vector2d(1,2);
+        assertEquals(expected,animal.getPosition());
+    }
+    @Test
+    void move2() {
+        RectangularMap map = new RectangularMap(4,4);
+        Animal animal=new Animal(new Vector2d(1,0));
+        try {
+            map.place(animal);
+        }catch (PositionAlreadyOccupiedException ex){
+            System.err.println(ex.getMessage());
+        }
+        map.move(animal,MoveDirection.BACKWARD);
+        Vector2d expected=new Vector2d(1,0);
+        assertEquals(expected,animal.getPosition());
+    }
+
+    @Test
+    void isOccupied() {
+        RectangularMap map = new RectangularMap(4,4);
+        Animal animal=new Animal(new Vector2d(1,0));
+        try {
+            map.place(animal);
+        }catch (PositionAlreadyOccupiedException ex){
+            System.err.println(ex.getMessage());
+        }
+        assertTrue(map.isOccupied(new Vector2d(1,0)));
+    }
+
+    @Test
+    void isOccupied2() {
+        RectangularMap map = new RectangularMap(4,4);
+        Animal animal=new Animal(new Vector2d(1,0));
+        try {
+            map.place(animal);
+        }catch (PositionAlreadyOccupiedException ex){
+            System.err.println(ex.getMessage());
+        }
+        assertFalse(map.isOccupied(new Vector2d(1,1)));
+    }
+    @Test
+    void objectAt() {
+        RectangularMap map = new RectangularMap(4,4);
+        Animal animal=new Animal(new Vector2d(1,0));
+        try {
+            map.place(animal);
+        }catch (PositionAlreadyOccupiedException ex){
+            System.err.println(ex.getMessage());
+        }
+        assertEquals(map.objectAt(new Vector2d(1,0)),animal);
+    }
+
+    @Test
+    void moveAnimalOutsideMapBounds() {
+        RectangularMap map = new RectangularMap(5, 5);
+        Animal animal = new Animal(new Vector2d(0, 0));
+        try {
+            map.place(animal);
+            animal.move(MoveDirection.BACKWARD, map); // Próba przemieszczenia się poza mapę
+            assertEquals(new Vector2d(0, 0), animal.getPosition()); // Zwierzę powinno pozostać na miejscu
+        } catch (PositionAlreadyOccupiedException ex) {
+            fail("Unexpected exception: " + ex.getMessage());
+        }
+    }
+
+    @Test
+    void moveAnimalToOccupiedPosition() {
+        RectangularMap map = new RectangularMap(5, 5);
+        Animal animal1 = new Animal(new Vector2d(2, 2));
+        Animal animal2 = new Animal(new Vector2d(2, 3));
+        try {
+            map.place(animal1);
+            map.place(animal2);
+            animal1.move(MoveDirection.FORWARD, map); // Próba przemieszczenia się na zajętą pozycję
+            assertEquals(new Vector2d(2, 2), animal1.getPosition()); // Zwierzę powinno pozostać na miejscu
+        } catch (PositionAlreadyOccupiedException ex) {
+            fail("Unexpected exception: " + ex.getMessage());
+        }
     }
 }
