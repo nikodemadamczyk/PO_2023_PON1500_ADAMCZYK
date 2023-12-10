e6e8d7cac1ef55519e4cb723d18aa3960d1a7ae5
Nikodem Adamczyk <adamczyknikus@gmail.com>
2023-12-10 20:49:05 +0100

    MAP in GUI v1

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -1,8 +1,32 @@
 package agh.ics.oop.presenter;
 
 import agh.ics.oop.model.WorldMap;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class SimulationPresenter {
+    private WorldMap worldMap;
+
+    @FXML
+    private Label infoLabel;
+
     public void setWorldMap(WorldMap worldMap) {
+        this.worldMap = worldMap;
+        drawMap();
+    }
+
+    public void drawMap() {
+        // Tutaj implementacja tłumaczenia mapy na postać siatki kontrolek
+        // Na razie ustawiamy zawartość mapy jako tekst
+        if (worldMap != null) {
+            infoLabel.setText(worldMap.toString());
+        } else {
+            infoLabel.setText("Mapa nie została jeszcze zainicjowana.");
+        }
+    }
+
+    // Metoda wywoływana, gdy mapa się zmienia
+    public void mapChanged() {
+        drawMap();
     }
 }
